369949de5e709b26833ba6197c716066db4094be
Rafael souza <156112067+NOOBSARAFA@users.noreply.github.com>
2025-04-10 16:06:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,16 @@ function togglemode(){
   const html = document.documentElement
   html.classList.toggle('light')
   
+const img = document.querySelector("#profile img")
+
+if (html.classList.contains("light"))
+{
+img.setAttribute("src", "./Avatar.png")
+  
+}
+else {img.setAttribute("src", "./avatarlondon.png")
+
+}
+
+
   }
